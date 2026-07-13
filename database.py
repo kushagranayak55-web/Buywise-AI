2eae7137b959a896250157a712afb406d4cac6da
Kushagra Nayak <Kushagranayak55@gmail.com>
2026-07-13 22:49:10 +0530

    Debug databse url

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
+from dotenv import load_dotenv
+load_dotenv()
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:review123@localhost:5432/buywise_ai"
+DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL = "DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 
 SessionLocal= sessionmaker(
